4b9b730a9d9a36e3e391a78001b0b6cd2ba1f647
minhhieu45 <vmhsky@gmail.com>
2022-10-21 20:50:25 +0700

    day 2

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,9 @@ const express = require('express');
 const route = express.Router();
 
 route.get('/', async (req, res, next) =>{
-    res.render('home');
+    const data = req.context;
+    console.log(data);
+    res.render('home', data);
 });
 route.get('/blog', async (req, res, next) =>{
     res.render('blog');
@@ -13,6 +15,9 @@ route.get('/menu', async (req, res, next) =>{
 });
 route.get('/reservation', async (req, res, next) =>{
     res.render('reservation');
+});
+route.get('/blog-single', async (req, res, next) =>{
+    res.render('blog-single');
 });
 
 
